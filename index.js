afdd0a203efa7cf43d49f1a0480adb47aebc7337
Thomas.G <gentilhomme.thomas@gmail.com>
2023-01-24 20:43:53 +0100

    fix(ci): remove lint stage and refactor script to run linter in test (#34)
    * fix(ci): remove lint stage and refactor script to run linter in test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import path from "path";
 import fs from "fs/promises";
 
 // Import Third-party Dependencies
-import conformance from "@nodesecure/licenses-conformance";
+import { licenseIdConformance } from "@nodesecure/licenses-conformance";
 
 // Import Internal Dependencies
 import { parsePackageLicense, getLicenseFromString } from "./src/utils.js";
@@ -18,7 +18,7 @@ async function parseLicense(dest) {
 
   const packageStr = await fs.readFile(path.join(dest, "package.json"), "utf-8");
   const detectedName = parsePackageLicense(JSON.parse(packageStr));
-  const license = conformance(detectedName);
+  const license = licenseIdConformance(detectedName);
   uniqueLicenseIds.push(...license.uniqueLicenseIds);
   license.from = "package.json";
   licenses.push(license);
@@ -32,7 +32,7 @@ async function parseLicense(dest) {
     const str = await fs.readFile(path.join(dest, file), "utf-8");
     const licenseName = getLicenseFromString(str);
     if (licenseName !== "unknown license") {
-      const license = conformance(licenseName);
+      const license = licenseIdConformance(licenseName);
       if (Reflect.has(license, "error")) {
         continue;
       }
